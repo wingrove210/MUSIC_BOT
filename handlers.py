cc794a50031f4cc0ac630ce165bb30f210a1cfb4
Vlad Dremkov <vladidermkov330@gmail.com>
2025-02-21 23:53:09 +0300

    modified:   handlers.py 	new file:   svo_photo.jpg

--- handlers.py
+++ handlers.py
@@ -7,9 +7,11 @@ router = Router()
 # Обработчик команды /start
 @router.message(CommandStart())
 async def cmd_start(message: types.Message):
-    await message.answer("""Привет!
+    caption = """Привет!
 Мы команда профессионалов, создаём песни для героев, превращая их истории в музыку.
 
 Вы можете подарить песню близкому на фронте или, находясь на передовой, передать его родным. Музыка навсегда увековечит историю и имя героя. 
 
-Жми на старт – и мы создадим для вас песню""")
+Жми на старт – и мы создадим для вас песню"""
+    photo = types.InputFile("svo_photo.jpg")
+    await message.answer_photo(photo=photo, caption=caption)
